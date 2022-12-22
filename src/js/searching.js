cb9fb42e484942f179cac835de6d00d1697a516d
NEXT1990 <maksim.listunov@gmail.com>
2022-12-22 21:45:40 +0200

    add

--- src/js/searching.js
+++ src/js/searching.js
@@ -1,1 +1,12 @@
 import axios from 'axios';
+
+const API_KEY = `32251243-fbd11de6fcf1160e262026676`;
+function serchPhoto(event) {
+  const urlApi = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
+  return fetch(urlApi).then(res => {
+    if (res.status !== 200) {
+      console.log('Все что угодно, но не 200!');
+    }
+    return res.json();
+  });
+}
