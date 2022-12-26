039566ed779b5300d9edc1431ca31bb9b3d0d646
NEXT1990 <maksim.listunov@gmail.com>
2022-12-27 01:07:18 +0200

    add-hw

--- src/index.js
+++ src/index.js
@@ -1,17 +1,67 @@
 import './css/styles.css';
 
 import { Notify } from 'notiflix';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-import fetchPhotos from './js/searching';
-const form = document.getElementById('search-form');
-const gallery = document.querySelector('.gallery');
+import { PicturesApi } from './js/searching';
 
-form.addEventListener('submit', onSubmit);
+const refs = {
+  form: document.getElementById('search-form'),
+  gallery: document.querySelector('.gallery'),
+};
 
-function onSubmit(event) {
+refs.form.addEventListener('submit', onSubmit);
+
+const PictureApi = new PicturesApi();
+
+async function onSubmit(event) {
   event.preventDefault();
   const formElements = event.currentTarget.elements;
   const request = formElements.searchQuery.value;
-  fetchPhotos(request);
+  PictureApi.query = request.trim();
+  console.log(PictureApi.query);
+  if (PictureApi.query === '') {
+    Notify.warning("Searching can't be empty");
+    return;
+  }
+  PictureApi.resetPage();
+
+  try {
+    const { hits, totalHits } = await PictureApi.responseApi();
+    render(hits);
+  } catch (error) {
+    Notify.failure('Something bad');
+  }
+}
+
+function render(hits) {
+  const images = hits
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return ` <div class="photo-card">
+      <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b> ${likes}
+          </p>
+          <p class="info-item">
+            <b>Views</b> ${views}
+          </p>
+          <p class="info-item">
+            <b>Comments</b> ${comments}
+          </p>
+          <p class="info-item">
+            <b>Downloads</b> ${downloads}
+          </p>
+          </div>`;
+      }
+    )
+    .join('');
+  refs.gallery.insertAdjacentHTML('beforeend', images);
 }
--- src/js/searching.js
+++ src/js/searching.js
@@ -1,16 +1,52 @@
 import axios from 'axios';
 
-const API_KEY = '32251243-fbd11de6fcf1160e262026676';
-let q = '';
-let page = 1;
-const perPage = 40;
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+export class PicturesApi {
+  constructor() {
+    this.key = '32251243-fbd11de6fcf1160e262026676';
+    this.query = '';
+    this.page = 1;
+    this.perPage = 40;
+  }
 
-export default async function fetchPhotos(request) {
-  const urlApi = `https://pixabay.com/api/?key=${API_KEY}&q=${request}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
-  await axios(urlApi).then(res => {
-    if (res.status !== 200) {
-      console.log('Все что угодно, но не 200!');
-    }
-    return res;
-  });
+  async responseApi() {
+    const options = new URLSearchParams({
+      key: this.key,
+      q: this.query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: this.page,
+      per_page: this.perPage,
+    });
+    const { data } = await axios(`?${options}`);
+    this.page += 1;
+    return data;
+  }
+
+  /**
+   * @param {string} request
+   */
+  set setQuery(request) {
+    this.query = request;
+  }
+
+  get getQuery() {
+    return this.query;
+  }
+
+  resetPage() {
+    this.page = 1;
+  }
 }
+
+// export default async function fetchPhotos(request) {
+//   const urlApi = `https://pixabay.com/api/?key=${API_KEY}&q=${request}&page=${page}&per_page=${perPage}&image_type=photo&orientation=horizontal&safesearch=true`;
+//   await axios(urlApi).then(res => {
+//     if (res.status !== 200) {
+//       console.log('Все что угодно, но не 200!');
+//     }
+//     console.log(res.data);
+//     return res;
+//   });
+// }
